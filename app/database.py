f16467373e66bdb7a36890025317f982e5895245
Gabriel Johnson <johnfem4real@gmail.com>
2026-01-29 18:09:47 +0100

    Add changes to database

--- app/database.py
+++ app/database.py
@@ -9,6 +9,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable not set")
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

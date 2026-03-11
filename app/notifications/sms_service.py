59ec99d0b295fc9a9de9214b579a2105c6b982b9
Gabriel Johnson <johnfem4real@gmail.com>
2026-03-11 19:52:46 +0100

    testing

--- app/notifications/sms_service.py
+++ app/notifications/sms_service.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 from app.core.config import settings
 
 
@@ -20,4 +20,28 @@ def send_sms(phone_number: str, message: str):
     try:
         requests.post(TERMII_URL, json=payload, timeout=10)
     except Exception as e:
-        print("SMS sending failed:", e)
+        print("SMS sending failed:", e) """
+
+import requests
+from app.core.config import settings
+
+TERMII_URL = "https://api.termii.com/api/sms/send"
+
+
+def send_sms(phone_number: str, message: str):
+
+    payload = {
+        "to": phone_number,
+        "from": "SALESZY",
+        "sms": message,
+        "type": "plain",
+        "channel": "generic",
+        "api_key": settings.TERMII_API_KEY,
+    }
+
+    print("Sending SMS to:", phone_number)
+    print("Payload:", payload)
+
+    response = requests.post(TERMII_URL, json=payload)
+
+    print("Termii response:", response.text)

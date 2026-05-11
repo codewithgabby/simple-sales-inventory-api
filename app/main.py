c69a1b0e9b2979b72edf673373ca7a85a1a54e72
Gabriel Johnson <johnfem4real@gmail.com>
2026-05-11 23:40:59 +0100

    fix admin

--- app/main.py
+++ app/main.py
@@ -58,7 +58,9 @@ app.add_middleware(
         "api.saleszy.com.ng",
         "localhost",
         "127.0.0.1",
-        "simple-sales-inventory-api-production.up.railway.app"
+        "simple-sales-inventory-api-production.up.railway.app",
+        "saleszy.com.ng/admin.html",
+        "127.0.0.1:5500/admin.html"
     ],
 )
 
